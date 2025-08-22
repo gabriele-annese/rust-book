7b2fd60c317a30665f17c5621dcf96679da2de57
Gabriele Annese <gannese06@gmail.com>
2025-08-22 07:52:52 +0200

    add ownership project Fri 22 Aug 2025 07:52:52 CEST

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,6 @@
 /*
-    Ownership Rust's most unique feature, and it enables Rust to make memory safety guarantees without needing a garbage collector.
+    Ownership Rust's most unique feature, and it enables Rust to make memory safety guarantees 
+    without needing a garbage collector.
     In this chapter, we’ll talk about ownership as well as several related features: borrowing, slices, and how Rust lays data out in memory.
 */
 
